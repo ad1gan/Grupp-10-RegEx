d7680e472c5a421041ef7c0e606b31a1c37b102c
LukasJu <lukas.juschka@rwth-aachen.de>
2016-05-14 00:27:54 +0200

    Compilable version of automat recursion
    Also...
    - added a (actually useless) boolean whether you can end on that
    - changed Pair to be int based
    - added new Syntaxbaum features (at the dummy)

--- Syntaxbaum.java
+++ Syntaxbaum.java
@@ -3,4 +3,27 @@ public class Syntaxbaum{
 	public int size(){
 		return this.size;
 	}
+	public Syntaxbaum(){
+		size = 1;
+	}
+	public Syntaxbaum(String expression){
+		size = 2;
+	}
+	public char getValue(){
+		return 'A';
+	}
+	public Syntaxbaum getLeft(){
+		return this;
+	}
+	public Syntaxbaum getRight(){
+		return this;
+	}
 }
+
+/*
+Anforderungen:
+size
+hatKinder
+getValue
+
+*/
