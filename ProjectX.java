de232f05b602abc68910c1aa32ccb06861227c7a
LukasJu <lukas.juschka@rwth-aachen.de>
2016-06-09 12:28:13 +0200

    Useless

--- ProjectX.java
+++
@@ -1,8 +0,0 @@
-public class ProjectX{
-	public static void main(String[] args) {
-		/*Tree oak = new Tree(args[0]);
-		Automaton entron = new Automaton(oak);
-		RegexMatchResult douche = simulateBFS(args[1], entron);
-		douche.print();*/
-	}
-}
